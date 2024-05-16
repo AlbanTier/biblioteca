562f2b186cbb9d5bfb5746f156bde16774910523
AlbanTier <albannils03@gmail.com>
2024-05-16 16:14:34 +0200

    feat: add password length validation + notice

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates_confirmation_of :password_digest
   validates :email, format: URI::MailTo::EMAIL_REGEXP
+  validates :password, presence: true, length: { minimum: 6, message: "doit comporter au moins 6 caractères" }
   has_many :borrows
 end
