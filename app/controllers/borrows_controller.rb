f14717c32e7dd73efd3fe41900c5773fff8831cb
AlbanTier <albannils03@gmail.com>
2024-05-16 16:05:30 +0200

    wip

--- app/controllers/borrows_controller.rb
+++ app/controllers/borrows_controller.rb
@@ -4,14 +4,19 @@ class BorrowsController < ApplicationController
     @borrows_history = Borrow.includes(:user, :book).where.not(ended_at: nil)
     @still_borrow = Borrow.includes(:user, :book).where(ended_at: nil)
   end
+
+  def user_borrows
+    @borrows_history = current_user.borrows.includes(:book).where.not(ended_at: nil)
+    @still_borrow = current_user.borrows.includes(:book).where(ended_at: nil)
+  end
   def create
     @book = Book.find(params[:book_id])
     if check_availability(params[:book_id])
       @borrow = Borrow.new(book_id: params[:book_id], user_id: session[:user_id], started_at: Time.now)
       if @borrow.save
-        redirect_to @book, notice: "Commande prise en compte."
+        redirect_to @book, notice: "Réservation prise en compte."
       else
-        render 'books/show', alert: "Impossible de prendre en compte la commande."
+        render 'books/show', alert: "Impossible de prendre en compte la réservation."
         puts @borrow.errors.inspect
       end
     else
